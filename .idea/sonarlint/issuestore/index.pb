65c639795f1c8b7bf9dd220486cca266a2b3d8df
Back <1300657068@qq.com>
2021-06-08 16:21:08 +0800

    Author

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+?
+CONTRIBUTING.md,3\f\3f454a98e586d1aa0d322e19afd5e67e08f2d3c8
+=
+composer.json,3\9\3957b91a0d8450cea0115cfd5af69eac90d75897
+{
+Kvendor/marcin-orlowski/laravel-api-response-builder/src/ResponseBuilder.php,d\d\dd3a8416fe4348cfa1482fc4405e7dc3b2a3384c
+I
+src/ApiResponseFacade.php,1\4\141462be0d09e71c4da5c0daa1e7ee20bf1196b3
+<
+CHANGELOG.md,a\b\ab09011fa121d0a2bb9fa4ca76094f2482b902b7
--- src/ResponseHandler.php
+++ src/ResponseHandler.php
@@ -8,7 +8,7 @@ trait ResponseHandler
     /**
      * Notes:[成功通知]
      * Desc: 这个方法，必须传递 data 数据，msg 可以自己进行控制
-     * User: COJOY_10
+     * User: Back
      * Date: 2021/6/4
      * Time: 18:19
      * @param  array  $data
@@ -23,7 +23,7 @@ trait ResponseHandler
     /**
      * Notes:[自定义成功消息通知]
      * Desc: 这个方法，默认返回成功。data 数据默认为 null，msg 信息自行控制
-     * User: COJOY_10
+     * User: Back
      * Date: 2021/6/4
      * Time: 18:19
      * @param $msg
@@ -37,7 +37,7 @@ trait ResponseHandler
     /**
      * Notes:[错误请求]
      * Desc: 错误方法，可以传递 api_code，系统根据 api_code 自行查找对应的文字说明，具体看你是否配置了文字对应关系
-     * User: COJOY_10
+     * User: Back
      * Date: 2021/6/4
      * Time: 18:20
      * @param $api_code
@@ -51,7 +51,7 @@ trait ResponseHandler
     /**
      * Notes:[错误提示]
      * Desc: 错误方法，可以传递 api_code，系统根据 api_code 自行查找对应的文字说明，可以控制 http_code 必传。比如 人脸识别失败，我要返回 200 状态码。
-     * User: COJOY_10
+     * User: Back
      * Date: 2021/6/4
      * Time: 18:20
      * @param $api_code
@@ -66,7 +66,7 @@ trait ResponseHandler
     /**
      * Notes:[账号密码验证失败]
      * Desc: 账号密码错误，或者登录失效都可以是用此方法
-     * User: COJOY_10
+     * User: Back
      * Date: 2021/6/4
      * Time: 18:20
      * @param $api_code
@@ -80,7 +80,7 @@ trait ResponseHandler
     /**
      * Notes:[数据不存在]
      * Desc: 数据不存在时可以使用此方法
-     * User: COJOY_10
+     * User: Back
      * Date: 2021/6/4
      * Time: 18:20
      * @param $api_code
